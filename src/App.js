d84cbef6cb529e6f9fdb1c4af3a25a326ac97b34
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-11-01 18:56:03 +0300

    almost done!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import Header from "./components/Header";
-import { Box, Container, CssBaseline, Grid } from "@mui/material";
+import {
+  Box,
+  Container,
+  createTheme,
+  CssBaseline,
+  Grid,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material";
 import ProjectCard from "./components/ProjectCard";
 import projects from "./projects.json";
 
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container>
         <Box mt={7}>
@@ -16,19 +27,20 @@ function App() {
         <Grid container spacing={7} mt={4} mb={15} alignItems={"stretch"}>
           {projects.map((project) => {
             return (
-              <Grid item xs={12} lg={6} key={Math.random()}>
+              <Grid item xs={12} md={6} key={project.title}>
                 <ProjectCard
                   title={project.title}
                   description={project.description}
                   github={project.github}
                   site={project.site}
+                  image={project.image}
                 />
               </Grid>
             );
           })}
         </Grid>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
